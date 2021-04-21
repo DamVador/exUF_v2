3086c09645fa268234293063212fc7f7b8e18ef8
Damien <damienbanville@hotmail.fr>
2021-04-21 17:05:34 +0200

    company controller added

---
+++ app/sprinkles/site/src/Controller/CompaniesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace UserFrosting\Sprinkle\Site\Controller;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\NotFoundException;
+use UserFrosting\Sprinkle\Core\Controller\SimpleController;
+use UserFrosting\Support\Exception\ForbiddenException;
+use UserFrosting\Sprinkle\Site\Database\Models\Company;
+use UserFrosting\Sprinkle\Core\Facades\Debug;
+
+class CompaniesController extends SimpleController
+{
+    public function pageList(Request $request, Response $response, $args)
+    {   
+        $companies = Company::all();
+        return $this->ci->view->render($response, 'pages/companies.html.twig', [
+            'companies' => $companies
+        ]);
+    }
+}
